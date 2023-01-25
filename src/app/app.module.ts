19fdae7dce64e9c1e30c70b5a67131220338f943
toth julia <t.julia982@gmail.com>
2023-01-25 09:15:53 +0100

    kezdés

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DiamondComponent } from './diamond/diamond.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DiamondComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/diamond/diamond.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-diamond',
+  templateUrl: './diamond.component.html',
+  styleUrls: ['./diamond.component.scss']
+})
+export class DiamondComponent {
+
+}
